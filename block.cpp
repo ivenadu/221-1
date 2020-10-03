75a517378d589c62ae31dd0785c26aa59a7a2994
ivena <ivena2012@gmail.com>
2020-10-03 14:19:06 -0700

    commit

---
+++ block.cpp
@@ -0,0 +1,28 @@
+#include "block.h"
+#include <cmath>
+#include <iostream>
+
+int Block::width() const
+{
+/* your code here */
+}
+
+int Block::height() const
+{
+/* your code here */
+}
+
+void Block::render(PNG &im, int column, int row) const
+{
+/* your code here */
+}
+
+void Block::build(PNG &im, int column, int row, int width, int height)
+{
+/* your code here */
+}
+
+void Block::flip()
+{
+/* your code here */
+}
